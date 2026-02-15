e53a4466fbab1e8fdafe5634bfd704125283b161
Khalid <khalidbenarfota@gmail.com>
2026-02-15 08:22:22 +0200

    modifies the file Product components

--- frontEnd/src/pages/products/Product.tsx
+++ frontEnd/src/pages/products/Product.tsx
@@ -4,7 +4,7 @@ import { HeartIcon } from "./HeartIcon";
 
 function Product({ product }: Readonly<{ product: ProductType }>) {
   return (
-    <div className="sm:ml-8 p-3 relative">
+    <div className="sm:ml-8 p-3 relative hover:bg-gray-700">
       <div className="relative">
         <img
           src={product.image}
@@ -17,7 +17,7 @@ function Product({ product }: Readonly<{ product: ProductType }>) {
       <div className="p-4">
         <Link to={`/product/${product._id}`}>
           <h2 className="flex justify-between items-center">
-            <div className="text-lg">{product.name}</div>
+            <div className="text-lg">{product.name.slice(0, 23) + "..."}</div>
             <span className="bg-pink-100 text-pink-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-lg dark:bg-pink-900 dark:text-pink-300">
               $ {product.price}
             </span>
